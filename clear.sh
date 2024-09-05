8ba39db3447fa89c7d2f7152c90d3fba87710dbd
IsacPasianotto <yh4mop12z@gmail.com>
2024-09-05 16:44:15 +0200

    Added first part of chapter 4

--- clear.sh
+++ clear.sh
@@ -1,4 +1,13 @@
-rm main.aux main.out main.log main.run.xml main.pdf main.toc main.bbl main.blg main-blx.bib
+rm main.aux \
+   main.out \
+   main.log \
+   main.run.xml \
+   main.pdf \
+   main.toc \
+   main.bbl \
+   main.blg \
+   main-blx.bib \
+   pdfa.xmpi
 find ./ -type d -name "auto" -exec rm -rf {} +
 rm -rf _minted-main
 
